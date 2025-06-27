c69e287b7c80d9cf2aa6ed6f27611c0c195a0693
BallerJay <i@ballerjay.cc>
2025-06-27 10:48:53 +0800

    Revert "✨ 增加WhenOff类和JSON文件"

--- test/index.test.ts
+++
@@ -1,8 +0,0 @@
-import WhenOff from '../src';
-
-const whenOff = new WhenOff();
-console.log('🎉', whenOff.isHoliday(new Date('2025-02-04')), '是否节假日');
-console.log('🎉', whenOff.isWorkingDay(new Date('2025-02-08')), '是否工作日');
-console.log('💼', whenOff.isAlternateWorkDay(new Date('2025-01-26')), '是否补班日');
-console.log('🎉🎉', whenOff.getDateInfo(new Date('2025-01-29')));
-console.log('🎉🎉', whenOff.getHolidayStats(), '节假日统计');
